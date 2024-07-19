f452d020cf1fd8ef87b821e055653a7eb255608c
adamatdevops <adamatdevops@gmail.com>
2024-07-19 11:35:17 +0300

    Family Avatar bugfix

--- src/components/ui/avatars/FamilyAvatar.tsx
+++ src/components/ui/avatars/FamilyAvatar.tsx
@@ -2,13 +2,14 @@
 import React from 'react';
 import { Avatar } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
-import FamilyIcon from '../icons/familyIcon.tsx';
+// import FamilyIcon from '../icons/familyIcon.tsx';
+import FamilyIcon from '../../../assets/family-avatar.svg';
 import styles from './css/FamilyAvatar.module.css';
 
 const FamilyAvatar: React.FC = () => (
     <Avatar
         shape="circle"
-        src="/src/assets/family-avatar.svg"
+        src={FamilyIcon}
         // size={32}
         className={styles.avatar}
         icon={<UserOutlined />}>
